8fe844e86d7731b8138bf02dc452244b81129a80
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-09-06 22:11:22 +0530

    done basics

---
+++ Basics/Hello.py
@@ -0,0 +1,15 @@
+# print("hello python")
+
+print(10%5)
+
+num = 10
+num1= 20
+
+numbers = [1,2,3,4,5,6,7,8]
+
+
+def Old(n):
+    print(n)
+
+    
+Old(5)
---
+++ Basics/second.py
@@ -0,0 +1,4 @@
+from Hello import Old
+
+
+Old("hanji")
